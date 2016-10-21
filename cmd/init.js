fec4196472efb9178308a542280a720d497e416c
luozaichun <952467274@qq.com>
2016-10-21 14:49:24 +0800

    fix bug

--- cmd/init.js
+++ cmd/init.js
@@ -5,57 +5,55 @@ const config = require('../templates');
 const chalk = require('chalk');
 const fs = require('fs');
 const path = require("path");
-
 module.exports = () => {
     co(function *() {
-        // 处理用户输入
+        /*处理用户输入*/
         let tplName = yield prompt('Template name: ');
         let projectName = yield prompt('Project name: ');
-        let dirArry=yield config.tpl[tplName]['dir'].split(",");
-        let _path=__dirname;
+        let _path=process.cwd();
         if (!config.tpl[tplName]) {
             console.log(chalk.red('\n × Template does not exit!'));
             process.exit()
         }
-        console.log(chalk.white('\n Start Project...'));
-
-        // 创建所有目录
-        let dirpath=_path+projectName;
-        let mkdirs=(dirpath,callback)=> {
-            fs.exists(dirpath, function (exists) {
-                if (exists) {
-                    dirArry.forEach(v => {
-                        if (!fs.existsSync(dirpath + "/" + v)){
-                          let pathArry=v.split("/");
-                            pathArry.forEach((v,i)=> {
-                                var curPath=pathArry.slice(0,i+1).join('/');
-                                var isExist = fs.existsSync(dirpath+"/"+curPath);
-                                !isExist?fs.mkdirSync(dirpath+"/"+curPath):null;
-                            });
-                        }
-                       
-                    });
-                    console.log(chalk.green('\n √ Project completed!'));
-                    process.exit();
+        else{
+            console.log(chalk.white('\n Start Project...'));
+            let dirArry=yield config.tpl[tplName]['dir'].split(",");
+            /*创建所有目录*/
+            let dirpath=_path+"/"+projectName;
+            let mkdirs=(dirpath,callback)=> {
+                fs.exists(dirpath, function (exists) {
+                    if (exists) {
+                        dirArry.forEach(v => {
+                            if (!fs.existsSync(dirpath + "/" + v)){
+                                let pathArry=v.split("/");
+                                pathArry.forEach((v,i)=> {
+                                    var curPath=pathArry.slice(0,i+1).join('/');
+                                    var isExist = fs.existsSync(dirpath+"/"+curPath);
+                                    !isExist?fs.mkdirSync(dirpath+"/"+curPath):null;
+                                });
+                            }
+                        });
+                        console.log(chalk.green('\n √ Project completed!'));
+                        process.exit();
+                    }
+                    else {
+                        /*尝试创建父目录，然后再创建当前目录*/
+                        fs.mkdir(dirpath, err => {
+                            if(err){
+                                console.log(err);
+                                process.exit()
+                            }
+                            mkdirs(dirpath,callback);
+                        })
+                    }
+                });
+            };
+            mkdirs(dirpath,err=>{
+                if(err){
+                    console.log(err);
+                    process.exit()
                 }
-                else {
-                    //尝试创建父目录，然后再创建当前目录
-                    fs.mkdir(dirpath, err => {
-                        if(err){
-                            console.log(err);
-                            process.exit()
-                        }
-                        mkdirs(dirpath,callback);
-                    })
-                }
-            });
-        };
-        mkdirs(dirpath,err=>{
-            if(err){
-                console.log(err);
-                process.exit()
-            }
-        });
-        
+            });  
+        }
     })
 };
